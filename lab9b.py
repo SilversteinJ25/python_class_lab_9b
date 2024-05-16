dd7499b728870895ec60aa3cfec7b3f9353229af
SilversteinJ25 <silverjustine@gmail.com>
2024-05-16 11:01:39 -0500

    Create lab9b.py

---
+++ lab9b.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 16 09:53:54 2024
+
+@author: justinesilverstein
+"""
+
+class Agent ():
+    def __init__(self):
+        
+        pass
+    
+    def __move__(self):
+        
+        
+        pass
